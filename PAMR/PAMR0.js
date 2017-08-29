07b3a5cabf2eb5efa8498573ad68c13dded467a2
Adrien <adrien@aggery.com>
2017-08-29 12:23:55 +0200

    Added indexed Permutations to dynamically get the right one

--- PAMR/PAMR0.js
+++ PAMR/PAMR0.js
@@ -2,21 +2,22 @@ const m = require('mathjs')
 
 const Helper = require('./Helper')
 /* Change this according to portfolio size and desired step */
-const portfolios = require('./PortfolioPermutations/A2S1')
+const portfolios = require('./PortfolioPermutations').indexed
 
 const _portfolioScore = (b, bt) => 0.5 * m.pow(m.norm(m.subtract(b, bt)), 2)
 
 const PAMR0 = (bt, xt) => {
-    const bestPortfolio = portfolios.reduce((acc, b) => {
-        if (Helper.insensitiveLoss(b, xt) === 0) {
-            const score = _portfolioScore(b, bt)
-            if (!acc.score || score < acc.score) {
-                acc.portfolio = b
-                acc.score = score
+    const bestPortfolio = portfolios[bt.length][2] /* This selects the right permutations for the current portfolio */
+        .reduce((acc, b) => {
+            if (Helper.insensitiveLoss(b, xt) === 0) {
+                const score = _portfolioScore(b, bt)
+                if (!acc.score || score < acc.score) {
+                    acc.portfolio = b
+                    acc.score = score
+                }
             }
-        }
-        return acc
-    }, { portfolio: bt })
+            return acc
+        }, { portfolio: bt })
 
     return bestPortfolio.portfolio
 }
--- PAMR/PortfolioPermutations/index.js
+++ PAMR/PortfolioPermutations/index.js
@@ -3,9 +3,25 @@ const A2S2 = require('./A2S2')
 const A4S1 = require('./A4S1')
 const A4S2 = require('./A4S2')
 
-module.exports = {
+const indexed = [
+    [], [], [
+        null,
+        A2S1, /* indexed[2][1] */
+        A2S2 /* indexed[2][2] */
+    ], [], [
+        null,
+        A4S1, /* indexed[4][1] */
+        A4S2 /* indexed[4][2] */
+    ]
+]
+
+const all = {
     A2S1,
     A2S2,
     A4S1,
     A4S2
+}
+module.exports = {
+    indexed,
+    all
 }
