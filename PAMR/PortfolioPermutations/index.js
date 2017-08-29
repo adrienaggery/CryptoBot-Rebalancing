3b7f7a0865985786d18542b6134001dd27ec8608
Adrien <adrien@aggery.com>
2017-08-29 12:05:14 +0200

    added index for permutations in case we want them all

---
+++ PAMR/PortfolioPermutations/index.js
@@ -0,0 +1,11 @@
+const A2S1 = require('./A2S1')
+const A2S2 = require('./A2S2')
+const A4S1 = require('./A4S1')
+const A4S2 = require('./A4S2')
+
+module.exports = {
+    A2S1,
+    A2S2,
+    A4S1,
+    A4S2
+}
